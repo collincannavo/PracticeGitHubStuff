36391202e15c11990cd5ea58c176acc1f3d22e3e
Bop <collin.cannavo@gmail.com>
2017-07-24 11:55:48 -0600

    Added foo to ViewController.swift

---
+++ GitHubPractice/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GitHubPractice
+//
+//  Created by Collin Cannavo on 7/24/17.
+//  Copyright © 2017 Collin Cannavo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        // This is a change in the project
+        // Trying to change again
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
